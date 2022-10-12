5927553a9f22468497186d710127a0e7de4d6000
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-10-12 18:45:30 +0100

    start making mass matrices work

--- bernstein.py
+++ bernstein.py
@@ -1,12 +1,41 @@
 import numpy as np
 import scipy.special
-import typing
+
+
+def choose(n, r):
+    """Return n choose r."""
+    out = 1
+    for i in range(min(r, n - r)):
+        out *= (n - i) / (i + 1)
+    return out
+
+
+def multichoose(ns, rs):
+    if sum(ns) == sum(rs) == 0:
+        return 2
+    out = 1
+    for n, r in zip(ns, rs):
+        out *= choose(n, r)
+    return out
 
 
 def compute_moments_triangle(n, f, fdegree):
+    """Compute the Bernstein moments.
+
+    These are defined in equation (12) of https://doi.org/10.1137/11082539X
+    (Ainsworth, Andriamaro, Davydov, 2011).
+
+    Args:
+      n: The polynomial degree of the Bernstein polynomials.
+      f: The function to take moments with.
+      fdegree: The polynomial degree of the function f.
+
+    Returns:
+      A two-dimensional array containing the Bernstein moments.
+    """
     if fdegree == 0:
         return np.array(
-            [[f(0,0) / (n + 1) / (n + 2) for _ in range(n + 1)] for _ in range(n + 1)])
+            [[f(0, 0) / (n + 1) / (n + 2) for _ in range(n + 1)] for _ in range(n + 1)])
 
     rule1 = scipy.special.roots_jacobi((n + fdegree) // 2 + 1, 1, 0)
     rule2 = scipy.special.roots_jacobi((n + fdegree) // 2 + 1, 0, 0)
@@ -15,7 +44,6 @@ def compute_moments_triangle(n, f, fdegree):
 
     q = len(rule1[0])
     assert len(rule2) == len(rule1)
-
 
     f0 = np.array([
         [f(p1, p2 * (1 - p1)) for p2 in rule2[0]]
@@ -31,7 +59,7 @@ def compute_moments_triangle(n, f, fdegree):
                 f1[alpha1, i2] += ww * f0[i1, i2]
             ww *= r * (n - alpha1) / (1 + alpha1)
 
-    f2 = np.zeros((n+1, n+1))
+    f2 = np.zeros((n + 1, n + 1))
     for i2, (p, w) in enumerate(zip(*rule2)):
         s = 1 - p
         r = p / s
@@ -44,5 +72,33 @@ def compute_moments_triangle(n, f, fdegree):
     return f2
 
 
-def compute_mass_matrix_triangle():
-    pass
+def compute_mass_matrix_triangle(n, f=None, fdegree=0):
+    """
+    Compute the mass matrix with a weight function.
+
+    These method is described in section 4.1 of https://doi.org/10.1137/11082539X
+    (Ainsworth, Andriamaro, Davydov, 2011).
+
+    Args:
+      n: The polynomial degree of the Bernstein polynomials.
+      f: The function to take moments with. This is the function c from the paper.
+      fdegree: The polynomial degree of the function f.
+
+    Returns:
+      A mass matrix.
+    """
+    moments = compute_moments_triangle(2 * n, f, fdegree)
+
+    mat = np.zeros(((n + 1) * (n + 2) // 2, (n + 1) * (n + 2) // 2))
+
+    i = 0
+    for a in range(n + 1):
+        for b in range(n + 1 - a):
+            j = 0
+            for c in range(n + 1):
+                for d in range(n + 1 - c):
+                    mat[i, j] = multichoose([b + d, a + c], [b, a]) / choose(2 * n, n) * moments[b + d, a + c]
+                    j += 1
+            i += 1
+
+    return mat
--- test.py
+++ test.py
@@ -6,19 +6,21 @@ import pytest
 
 import bernstein
 
+x, y = sympy.Symbol("x"), sympy.Symbol("y")
+
 
 @pytest.mark.parametrize("px", range(4))
 @pytest.mark.parametrize("py", range(4))
 @pytest.mark.parametrize("n", range(1, 4))
 def test_integrals_triangle(px, py, n):
-    x, y = sympy.Symbol("x"), sympy.Symbol("y")
-
-    b = symfem.elements.bernstein.bernstein_polynomials(n, 2)
-
     def f(x, y):
         return x ** px * y ** py
 
-    integrals1 = [(i *  f(x, y)).integrate([x, 0, 1-y], [y, 0, 1]) for i in b]
+    b = symfem.elements.bernstein.bernstein_polynomials(n, 2)
+
+    integrals1 = [float(
+        (i *  f(x, y)).integrate([x, 0, 1-y], [y, 0, 1])
+    ) for i in b]
     integrals1 = [float(i) for i in integrals1]
 
     integrals2 = bernstein.compute_moments_triangle(n, f, px + py)
@@ -26,3 +28,26 @@ def test_integrals_triangle(px, py, n):
     integrals2 = [integrals2[j, i] for i in range(d) for j in range(d - i)]
 
     assert np.allclose(integrals1, integrals2)
+
+
+@pytest.mark.parametrize("px", range(3))
+@pytest.mark.parametrize("py", range(3))
+@pytest.mark.parametrize("n", range(1, 3))
+def test_mass_matrix(px, py, n):
+
+    def f(x, y):
+        return x ** px * y ** py
+
+    mass1 = bernstein.compute_mass_matrix_triangle(n, f, px + py)
+
+    b = symfem.elements.bernstein.bernstein_polynomials(n, 2)
+
+    mass2 = [[float(
+        (i * j *  f(x, y)).integrate([x, 0, 1-y], [y, 0, 1])
+    ) for i in b] for j in b]
+
+
+    print(mass1)
+    print(mass2)
+
+    assert np.allclose(mass1, mass2)
