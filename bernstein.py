d3fb5f7e213e00477b914589e04d7ca0c4bcd228
Chris Richardson <chris@bpi.cam.ac.uk>
2022-10-15 21:13:41 +0100

    flake8

--- bernstein.py
+++ bernstein.py
@@ -92,7 +92,7 @@ def compute_mass_matrix_triangle(n, f=None, fdegree=0):
     mat = np.zeros(((n + 1) * (n + 2) // 2, (n + 1) * (n + 2) // 2))
 
     # Pack index
-    idx = lambda i, j: (j*(2*n+3)-j*j)//2 + i
+    def idx(i, j): (j*(2*n+3)-j*j)//2 + i
 
     for a in range(n + 1):
         for b in range(n + 1):
@@ -100,7 +100,8 @@ def compute_mass_matrix_triangle(n, f=None, fdegree=0):
                 for b2 in range(n + 1 - b):
                     i = idx(a, a2)
                     j = idx(b, b2)
-                    mat[i, j] = choose(a + b, a) * choose(a2 + b2, a2) * choose(2*n - a - b - a2 - b2, n - a - a2) \
+                    mat[i, j] = choose(a + b, a) * choose(a2 + b2, a2) * \
+                        choose(2*n - a - b - a2 - b2, n - a - a2) \
                         * moments[a + b, a2 + b2]
 
     mat /= choose(2*n, n)
