ce85140147a936530b0b4c2a091b0826be06806c
Chris Richardson <chris@bpi.cam.ac.uk>
2025-01-27 18:44:01 -0700

    More GPU friendly loop order

--- bernstein.py
+++ bernstein.py
@@ -11,19 +11,21 @@ def evaluate_quad(dofs, q):
     ny = dofs.shape[1] - 1
     f0 = np.zeros((q, ny + 1))
     for iq, x in enumerate(qpts):
-        b = x**nx
         r = (1 - x) / x
-        for i in range(nx + 1):
-            f0[iq, :] += b * dofs[i, :]
-            b *= r * (nx - i)/(i + 1)
+        for j in range(ny + 1):
+            b = x**nx
+            for i in range(nx + 1):
+                f0[iq, j] += b * dofs[i, j]
+                b *= r * (nx - i)/(i + 1)
 
     qvals = np.zeros((q, q))
     for iq, x in enumerate(qpts):
-        b = x**ny
         r = (1 - x) / x
-        for j in range(ny + 1):
-            qvals[:, iq] += b * f0[:, j]
-            b *= r * (ny - j)/(j + 1)
+        for i in range(q):
+            b = x**ny
+            for j in range(ny + 1):
+                qvals[i, iq] += b * f0[i, j]
+                b *= r * (ny - j)/(j + 1)
 
     return qvals
 
@@ -36,22 +38,28 @@ def compute_moments_quad(qvals, nx, ny):
     qwts /= 2.0
 
     f1 = np.zeros((nx + 1, q))
-    for i1 in range(q):
-        x = qpts[i1]
-        r = (1 - x) / x
-        w = x**nx
-        for alpha1 in range(nx + 1):
-            f1[alpha1, :] += qwts[i1] * w * qvals[i1, :]
-            w *= r * (nx - alpha1)/(1 + alpha1)
+    for alpha1 in range(nx + 1):
+        for i1 in range(q):
+            x = qpts[i1]
+            r = (1 - x) / x
+            w = x**nx
+            for j in range(alpha1):
+                w *= r * (nx - j)/(1 + j)
+            for i2 in range(q):
+                f1[alpha1, i2] += qwts[i1] * w * qvals[i1, i2]
+
 
     f2 = np.zeros((nx + 1, ny + 1))
-    for i2 in range(q):
-        x = qpts[i2]
-        r = (1 - x) / x
-        w = x**ny
+    for alpha1 in range(nx + 1):
         for alpha2 in range(ny + 1):
-            f2[:, alpha2] += qwts[i2] * w * f1[:, i2]
-            w *= r * (ny - alpha2)/(1 + alpha2)
+            for i2 in range(q):
+                x = qpts[i2]
+                r = (1 - x) / x
+                w = x**ny
+                for j in range(alpha2):
+                    w *= r * (ny - j)/(1 + j)
+                f2[alpha1, alpha2] += qwts[i2] * w * f1[alpha1, i2]
+
 
     return f2
 
